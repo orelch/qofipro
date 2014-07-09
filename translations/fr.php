ef93a37b99b157c0746576bb1f59482404b73d6d
Aurélien LAJOIE <aurelien.lajoie@m4x.org>
2014-07-09 15:17:37 +0200

    Translations

---
+++ translations/fr.php
@@ -0,0 +1,14 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{qofipro}prestashop>qofipro_c8f333b87db375ec17517a8458675cf8'] = 'Premier module pour QOFIPro';
+$_MODULE['<{qofipro}prestashop>qofipro_0cbc6611f5540bd0809a388dc95a615b'] = 'Test';
+$_MODULE['<{qofipro}prestashop>qofipro_5cc8122405e1a58c8abe024d2a2ad312'] = 'Paramètres Evolubat';
+$_MODULE['<{qofipro}prestashop>qofipro_f337a90e3893814f24a7020cec406814'] = 'Url des webservices';
+$_MODULE['<{qofipro}prestashop>qofipro_de3794f1db0c4f6391878b4c68c99774'] = 'Utilisateur pour les webservices';
+$_MODULE['<{qofipro}prestashop>qofipro_997f24fa8fbafd90704b4cfd251dfea4'] = 'Mot de passe pour les webservices';
+$_MODULE['<{qofipro}prestashop>qofipro_c9cc8cce247e49bae79f15173ce97354'] = 'Sauver';
+$_MODULE['<{qofipro}prestashop>qofipro_630f6dc397fe74e52d5189e2c80f282b'] = 'Retour à la liste';
+$_MODULE['<{qofipro}prestashop>qofipro_cf3cd2d3cdbf33adc62e7207b67373fe'] = 'Url invalide';
+$_MODULE['<{qofipro}prestashop>qofipro_c888438d14855d7d96a2724ee9c306bd'] = 'Configuration mise à jour';
